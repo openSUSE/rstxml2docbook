e1fe06ae73f8fa8eceb61ece7baa26809816baca
Thomas Schraitle <toms@suse.de>
2016-01-20 10:17:56 +0100

    Start with tests

---
+++ test/test_version.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+def test_version():
+    """Check if version is available and set"""
+    from rstxml2db import __version__
+    assert __version__
+
+
+def test_version_from_cli(capsys):
+    """Checks if option --version creates a correct version"""
+    from rstxml2db import main
+    import pytest
+
+    with pytest.raises(SystemExit):
+        main(["--version"])
