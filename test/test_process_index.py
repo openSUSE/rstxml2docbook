0c310c89cc2fa6a8827f00b5af59d6de1243d697
Thomas Schraitle <toms@suse.de>
2016-01-20 10:51:40 +0100

    Add test cases for process_index

---
+++ test/test_process_index.py
@@ -0,0 +1,22 @@
+#
+
+from rstxml2db.treebuilder import process_index
+import os
+from py.path import local
+from lxml import etree
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+def test_process_index(tmpdir):
+    """
+    """
+    docname = 'doc.001.xml'
+    doc = local(HERE) / 'doc' / docname
+    doc.copy(tmpdir)
+    docfile = tmpdir / docname
+    treefile = tmpdir / 'tree.xml'
+    xml = process_index(str(docfile), str(treefile))
+    assert xml
+    assert os.path.exists(str(treefile))
+    assert xml.getroot().tag == 'ref'
+    assert xml.getroot().attrib['href'] == docname
