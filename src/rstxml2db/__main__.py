43c9745a0ea50b5ccc571ef83bb1eaf5789cc7b4
Thomas Schraitle <tom_schr@web.de>
2016-01-23 21:11:33 +0100

    Return exit code with sys.exit()

--- src/rstxml2db/__main__.py
+++ src/rstxml2db/__main__.py
@@ -16,7 +16,9 @@
 # To contact SUSE about this file by physical or electronic mail,
 # you may find current contact information at www.suse.com
 
+import sys
 from . import main
 
+
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
