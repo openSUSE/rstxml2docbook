626d6d82d602ecf2fab6e35f530258d3b968cdab
Thomas Schraitle <toms@suse.de>
2016-02-16 07:59:48 +0100

    Bump version: 0.4.0 → 0.4.1

--- src/rstxml2db/version.py
+++ src/rstxml2db/version.py
@@ -18,5 +18,5 @@
 
 """Contains __version__ and __author__"""
 
-__version__ = "0.4.0"  # flake8: noqa
+__version__ = "0.4.1"  # flake8: noqa
 __author__ = "Thomas Schraitle <toms (AT) suse DOT de>"  # flake8: noqa
