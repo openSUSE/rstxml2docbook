787299ea21f8bcd83557e40580939743ed4ce126
Thomas Schraitle <tom_schr@web.de>
2017-01-13 08:59:19 +0100

    Complete testcase for long and short CLI options

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -19,26 +19,51 @@ def test_prepareparams(params, expected):
 
 @pytest.mark.parametrize('cli,expected', [
   (['a.xml'], dict(indexfile='a.xml')),
+  #
   (['--verbose', 'a.xml'],
    dict(verbose=1)
    ),
   (['-vv', 'a.xml'],
    dict(verbose=2)
    ),
+  #
   (['-l', 'legal.xml', 'a.xml'],
    dict(legalnotice='legal.xml')
    ),
+  (['--legalnotice', 'legal.xml', 'a.xml'],
+   dict(legalnotice='legal.xml')
+   ),
+  #
   (['-c', 'preface.xml', 'a.xml'],
    dict(conventions='preface.xml')
    ),
+  (['--conventions', 'preface.xml', 'a.xml'],
+   dict(conventions='preface.xml')
+   ),
+  #
   (['-o', 'foo', 'a.xml'],
    dict(output='foo')
    ),
+  (['--output', 'foo', 'a.xml'],
+   dict(output='foo')
+   ),
+  #
   (['-k', 'a.xml'],
    dict(keepallids=False)
    ),
+  (['--keep-all-ids', 'a.xml'],
+   dict(keepallids=False)
+   ),
+  #
+  (['-N', 'Foo', 'a.xml'],
+   dict(_productname='Foo')
+   ),
   (['--productname', 'Foo', 'a.xml'],
    dict(_productname='Foo')
+   ),
+  #
+  (['-P', '42', 'a.xml'],
+   dict(_productnumber='42')
    ),
   (['--productnumber', '42', 'a.xml'],
    dict(_productnumber='42')
@@ -46,11 +71,19 @@ def test_prepareparams(params, expected):
   (['--productname', 'Foo', '--productnumber', '42', 'a.xml'],
    dict(_productname='Foo', _productnumber='42')
    ),
+  #
   (['-p', 'a=2', '-p', 'b=4', 'a.xml'],
    dict(params=[('a', '2'), ('b', '4')])
    ),
   (['--param', 'a=2', '--param', 'b=4', 'a.xml'],
    dict(params=[('a', '2'), ('b', '4')])
+   ),
+  #
+  (['-4', 'a.xml'],
+   dict(db4=True)
+   ),
+  (['--db4', 'a.xml'],
+   dict(db4=True)
    ),
 ])
 def test_parsecli(cli, expected):
